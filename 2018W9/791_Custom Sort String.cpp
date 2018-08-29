037dcfe49c3509818b5eb59942ec998b976fc769
Clarence Wang <chwangthu@gmail.com>
2018-08-30 00:13:52 +0800

    del

--- 2018W9/791_Custom Sort String.cpp
+++
@@ -1,9 +0,0 @@
-//Feb 25 2018
-
-class Solution {
-public:
-    string customSortString(string S, string T) {
-        sort(T.begin(), T.end(), [&](char a, char b){ return S.find(a) < S.find(b); });
-        return T;
-    }
-};
